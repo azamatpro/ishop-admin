cd0286be3526836e0c8f8cbabf95c715dfdb121d
Azamat Rasulov <azamat.rasulov00@gmail.com>
2024-09-20 15:49:40 +0900

    Update Login.js

--- src/pages/Authentication/Login.js
+++ src/pages/Authentication/Login.js
@@ -48,6 +48,7 @@ class Login extends Component {
         return;
       }
       navigate('/');
+      localStorage.setItem("authToken", "Fake_Token");
       this.props.dispatch(createShopSuccess(data));
       showAlert('success', 'You logged in your shop successfully!');
     } catch (error) {
